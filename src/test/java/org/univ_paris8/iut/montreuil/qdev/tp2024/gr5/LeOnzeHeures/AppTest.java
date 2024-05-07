ce6d05f28f4f2880930b53366395b87112ea3710
Bananabenj <benj.thibout@gmail.com>
2024-05-07 11:42:07 +0200

    Initialisation du projet, POM.xml etc

---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/LeOnzeHeures/AppTest.java
@@ -0,0 +1,18 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.LeOnzeHeures;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test 😊
+     */
+
+
+    @Test
+    public void shouldAnswerWithTrue(){assertTrue( true );}
+}
