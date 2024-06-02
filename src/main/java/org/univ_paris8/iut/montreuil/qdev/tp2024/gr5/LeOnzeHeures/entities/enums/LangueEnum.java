eb33381aaebc961fc84596bac64ff8624f42980e
Bananabenj <benj.thibout@gmail.com>
2024-06-02 12:51:06 +0200

    initialisation des DTO + Enum + interface JoueurService // push pré rendu, avant codage des differentes parties

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/LeOnzeHeures/entities/enums/LangueEnum.java
@@ -0,0 +1,10 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.LeOnzeHeures.entities.enums;
+
+public enum LangueEnum {
+    FRANCAIS,
+    ENGLISH,
+    DEUTSCH,
+    ESPANOL,
+    ITALIANO;
+}
+
